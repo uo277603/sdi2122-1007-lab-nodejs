82721d2afb2b90653814f1bd2a3bd9d0736916b2
uo277603 <uo277603@uniovi.es>
2022-04-04 11:36:44 +0200

    SDI-2122-1007-9.1-Gestión de colecciones, eliminar documento.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -43,6 +43,10 @@ app.use("/publications",userSessionRouter);
 app.use("/audios/",userAudiosRouter);
 app.use("/shop/",userSessionRouter)
 
+const userAuthorRouter = require('./routes/userAuthorRouter');
+app.use("/songs/edit",userAuthorRouter);
+app.use("/songs/delete",userAuthorRouter);
+
 
 app.set('connectionStrings', url);
 let songRepository = require("./repositories/songsRepository.js");
--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -30,6 +30,20 @@ module.exports = function (app, songsRepository, commentsRepository) {
         res.render("shop.twig", response);
     });
 
+    app.get('/songs/delete/:id', function (req, res) {
+        let filter = {_id: ObjectId(req.params.id)};
+        songsRepository.deleteSong(filter, {}).then(result => {
+            if (result == null || result.deletedCount == 0) {
+                res.send("No se ha podido eliminar el registro");
+            } else {
+                res.redirect("/publications");
+            }
+        }).catch(error => {
+            res.send("Se ha producido un error al intentar eliminar la canción: " + error)
+        });
+    });
+
+
     app.get('/shop', function (req, res) {
         let filter = {};
         let options = {sort: {title: 1}};
@@ -92,7 +106,7 @@ module.exports = function (app, songsRepository, commentsRepository) {
         let filterComments = {song_id: ObjectId(req.params.id)};
         songsRepository.findSong(filter, options).then(song => {
             commentsRepository.getComments(filterComments, {}).then(comments => {
-                res.render("songs/song.twig", {song: song, comments:comments});
+                res.render("songs/song.twig", {song: song, comments: comments});
             });
 
         }).catch(error => {
@@ -141,7 +155,6 @@ module.exports = function (app, songsRepository, commentsRepository) {
             }
         });
     });
-
 
 
     function step1UpdateCover(files, songId, callback) {
